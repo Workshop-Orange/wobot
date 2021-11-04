edfe4a5e864ecda9d79b278c454c9968937d2f42
Bryan Gruneberg <hello@bryangruneberg.com>
2021-11-04 05:31:58 -0400

    Wobot holdoff to 10 seconds

--- app/Commands/HorizonCommand.php
+++ app/Commands/HorizonCommand.php
@@ -90,8 +90,8 @@ class HorizonCommand extends Command
                 
                 if ($ret > 0) {
                     $engine->error("Step failed: " . $engine->getFailureClass() ." [" . $engine->getFailureCode() . "] " . $engine->getFailureMessage());
-                    $engine->info("Holding off for 8 seconds");
-                    sleep(8);
+                    $engine->info("Holding off for 10 seconds");
+                    sleep(10);
                 } else {
                     $engine->info("All steps completed successfully. Horizon can start.");
                     
