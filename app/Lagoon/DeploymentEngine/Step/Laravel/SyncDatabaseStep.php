8e61cf3d41f80dc8322778dcc867e1a21a6c88b6
Bryan Gruneberg <hello@bryangruneberg.com>
2021-11-05 05:45:24 -0400

    Changes to trigger DB anonymization

--- app/Lagoon/DeploymentEngine/Step/Laravel/SyncDatabaseStep.php
+++ app/Lagoon/DeploymentEngine/Step/Laravel/SyncDatabaseStep.php
@@ -4,12 +4,38 @@ namespace App\Lagoon\DeploymentEngine\Step\Laravel;
 
 use App\Lagoon\DeploymentEngine\Step\StepInterface;
 use App\Lagoon\DeploymentEngine\Step\StepBase;
+use Exception;
 use Illuminate\Support\Str;
 
 Class SyncDatabaseStep extends SyncBaseStep implements StepInterface
 {
     public function execute(): int
     {
-        return $this->executeSync("mariadb");
+        $syncRet = $this->executeSync("mariadb");
+
+        if($syncRet > 0) {
+            return $syncRet;
+        }
+
+        if(empty($this->config['anonymize']) || $this->config['anonymize'] != true) {
+            return $syncRet;
+        }
+
+        try {
+            $comRet = $this->engine->runLaravelArtisanCommand([
+             'db:anonymize'
+            ]);
+
+            if($comRet > 0) {
+                $this->setFailure(255, "Database anonymization failed");
+                return $this->getReturnCode();
+            }
+        } catch(Exception $ex) {
+            $this->setFailure(255, "Database anonymization failed: " . $ex->getMessage());
+            return $this->getReturnCode();
+        }
+
+        $this->engine->trackMilestoneProgress(class_basename($this), "Database anonymized successfully");
+        return 0;
     }
 }
